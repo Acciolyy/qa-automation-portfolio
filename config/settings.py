1437b2c46db9fd6521b569ef7265fa0cdbf3d896
Acciolyy <thiagoacciolyest@gmail.com>
2026-07-22 20:07:58 -0300

    layout: base.html com heranca de templates e CSS proprio
    Adiciona STATICFILES_DIRS e static/styles.css (CSS
      puro, sem framework). Cria templates/base.html com topo, navegacao e logout globais; converte as 6 telas para {% extends %}.
      Extras: data formatada na listagem, titulo dinamico no form de produto, botao de exclusao em destaque e links de retorno
      restaurados.

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Autenticação: para onde o Django manda o usuário
 LOGIN_REDIRECT_URL = 'home' # Depois de logar com sucesso
